ac772eb76d566538ebbb0fb7f786e1045d058f57
Reza82 <127042809+Rngpoor@users.noreply.github.com>
2023-04-24 22:51:35 +0330

    Update prediction.cs

--- Api project/prediction.cs
+++ Api project/prediction.cs
@@ -1,4 +1,6 @@
-﻿using currency4;
+using currency3;
+using currency4;
+using currency5;
 using System.Diagnostics;
 
 namespace currency6
@@ -11,33 +13,61 @@ namespace currency6
         public void prediction()
 
         {
-            string address = @"F:\currency data\time.text";
-            StreamReader streader = new StreamReader(address);
+            string address = @"C:\Users\IT CITY\source\repos\Api project\Api project\bin\Debug\net7.0\dsd.txt";
+            using (StreamReader streader = new StreamReader(address))
+            {
+                var line1 = Convert.ToDouble(streader.ReadLine());
+                var line2 = Convert.ToDouble(streader.ReadLine());
+                var line3 = Convert.ToDouble(streader.ReadLine());
+
+                var plus = line1 + line2 + line3;
 
 
-            double line1 = Convert.ToDouble(streader.ReadLine());
-            double line2 = Convert.ToDouble(streader.ReadLine());
-            double line3 = Convert.ToDouble(streader.ReadLine());
 
-            var plus = line1 + line2 + line3;
+                var average1 = plus / 3;
+
+                var currencyinfo = currencyprocess.currencyinformation();
+
+                var x = currencyinfo.Result.high;
+                var y = currencyinfo.Result.low;
+                var z = Convert.ToDouble( x + y);
+                var k = Convert.ToDouble(z) / 2;
+
+                var m = k - average1;
+                var predictonbuy = Convert.ToDouble(m + currencyinfo.Result.buy);
+
+                var pred =Convert.ToDouble(currencyinfo.Result.buy) -Convert.ToDouble( predictonbuy);
+
+                var Per = Convert.ToDouble(pred) / Convert.ToDouble( currencyinfo.Result.buy);
+                var percent =Per * 100;
+                
+                Console.Write("This is your possible percent of fault:   %");
+                Console.ForegroundColor = ConsoleColor.DarkRed;
+                Console.WriteLine(percent);
+                Console.ResetColor();
 
 
 
-            long average1 = Convert.ToInt32(plus) / 3;
-
-            var currencyinfo = currencyprocess.currencyinformation();
-
-            var q = currencyinfo.Result.high;
-            var e = currencyinfo.Result.low;
-            var r = q + e;
-            var t = Convert.ToInt32(r) / 2;
-
-            var m = t - average1;
-
-            var predictonbuy = m + currencyinfo.Result.buy;
 
 
-            Console.WriteLine("predicton  is:  {0}", predictonbuy);
+
+
+            }
+            
+
+
+
+        }
+
+
+
+
+
+    }
+}
+
+
+
 
         }
 
