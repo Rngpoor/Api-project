db5ce34f4d5f63778a9cb5d3d6d37567cb32e767
Reza82 <127042809+Rngpoor@users.noreply.github.com>
2023-04-24 22:50:11 +0330

    Update Program.cs

--- Api project/Program.cs
+++ Api project/Program.cs
@@ -1,18 +1,36 @@
-﻿// Reza naghipoor
+// Reza naghipoor
 
 
 using currency1;
 
 using currency4;
+using currency5;
+using currency6;
+using System.Security.Cryptography;
 
 ApiHelper.InitializeClient();
 var currencyinfo = await currencyprocess.currencyinformation();
 
 Console.Write($"this is your production:    ");
 
-Console.ForegroundColor = ConsoleColor.Red;
+Console.ForegroundColor = ConsoleColor.DarkYellow;
 Console.WriteLine(currencyinfo.sell);
 Console.ResetColor();
+Prediction Pred = new Prediction();
+Data data = new Data();
+do
+{
+    data.data();
+    Pred.prediction();
+
+
+
+} while (true);
+
+
+
+
+
 
 
 
